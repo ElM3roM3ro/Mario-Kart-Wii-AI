fd0f2666f89d866afd736f083a813059a79f4fbe
thomazac <thomaszachary8@gmail.com>
2025-03-26 18:18:35 -0500

    fixed noisy layer

--- agent.py
+++ agent.py
@@ -4,42 +4,42 @@ import logging
 
 # ----------------- NoisyLinear (with σ = 0.5) ----------------- #
 class NoisyLinear(nn.Module):
-    def __init__(self, in_features, out_features, sigma_init=0.5):  # changed sigma_init to 0.5
-        super(NoisyLinear, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight_mu = nn.Parameter(torch.FloatTensor(out_features, in_features))
-        self.weight_sigma = nn.Parameter(torch.FloatTensor(out_features, in_features))
-        self.register_buffer('weight_epsilon', torch.FloatTensor(out_features, in_features))
-        self.bias_mu = nn.Parameter(torch.FloatTensor(out_features))
-        self.bias_sigma = nn.Parameter(torch.FloatTensor(out_features))
-        self.register_buffer('bias_epsilon', torch.FloatTensor(out_features))
-        self.sigma_init = sigma_init
-        self.reset_parameters()
-        self.reset_noise()
-        
-    def reset_parameters(self):
-        mu_range = 1 / math.sqrt(self.in_features)
-        nn.init.uniform_(self.weight_mu, -mu_range, mu_range)
-        nn.init.constant_(self.weight_sigma, self.sigma_init / math.sqrt(self.in_features))
-        nn.init.uniform_(self.bias_mu, -mu_range, mu_range)
-        nn.init.constant_(self.bias_sigma, self.sigma_init / math.sqrt(self.in_features))
-        
-    def reset_noise(self):
-        device = self.weight_mu.device
-        eps_in = torch.randn(self.in_features, device=device).sign() * torch.sqrt(torch.abs(torch.randn(self.in_features, device=device)) + 1e-10)
-        eps_out = torch.randn(self.out_features, device=device).sign() * torch.sqrt(torch.abs(torch.randn(self.out_features, device=device)) + 1e-10)
-        self.weight_epsilon.copy_(eps_out.unsqueeze(1) * eps_in.unsqueeze(0))
-        self.bias_epsilon.copy_(eps_out)
-        
-    def forward(self, x):
-        if self.training:
-            weight = self.weight_mu + self.weight_sigma * self.weight_epsilon
-            bias = self.bias_mu + self.bias_sigma * self.bias_epsilon
-        else:
-            weight = self.weight_mu
-            bias = self.bias_mu
-        return nn.functional.linear(x, weight, bias)
+  def __init__(self, in_features, out_features, std_init=0.5):
+    super(NoisyLinear, self).__init__()
+    self.in_features = in_features
+    self.out_features = out_features
+    self.std_init = std_init
+    self.weight_mu = nn.Parameter(torch.empty(out_features, in_features))
+    self.weight_sigma = nn.Parameter(torch.empty(out_features, in_features))
+    self.register_buffer('weight_epsilon', torch.empty(out_features, in_features))
+    self.bias_mu = nn.Parameter(torch.empty(out_features))
+    self.bias_sigma = nn.Parameter(torch.empty(out_features))
+    self.register_buffer('bias_epsilon', torch.empty(out_features))
+    self.reset_parameters()
+    self.reset_noise()
+
+  def reset_parameters(self):
+    mu_range = 1 / math.sqrt(self.in_features)
+    self.weight_mu.data.uniform_(-mu_range, mu_range)
+    self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
+    self.bias_mu.data.uniform_(-mu_range, mu_range)
+    self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.out_features))
+
+  def _scale_noise(self, size):
+    x = torch.randn(size, device=self.weight_mu.device)
+    return x.sign().mul_(x.abs().sqrt_())
+
+  def reset_noise(self):
+    epsilon_in = self._scale_noise(self.in_features)
+    epsilon_out = self._scale_noise(self.out_features)
+    self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+    self.bias_epsilon.copy_(epsilon_out)
+
+  def forward(self, input):
+    if self.training:
+      return nn.functional.linear(input, self.weight_mu + self.weight_sigma * self.weight_epsilon, self.bias_mu + self.bias_sigma * self.bias_epsilon)
+    else:
+      return nn.functional.linear(input, self.weight_mu, self.bias_mu)
 
 # ----------------- BTR Network with IQN and Impala Blocks ----------------- #
 class BTRNetwork(nn.Module):
